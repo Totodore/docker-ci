5fe2e6afebf1b209935107bf488ce478bbc25c72
Elariondakta <prevottheodore@gmail.com>
2020-12-08 16:04:48 +0100

    pull image auth integration

--- src/docker.ts
+++ src/docker.ts
@@ -1,5 +1,6 @@
 import { DockerCiLabels } from './models/docker-ci-labels.model';
 import { DockerEventsModel } from './models/docker-events.model';
+import { DockerImagesModel } from './models/docker-images.model';
 import * as Docker from "dockerode";
 import { Logger } from "./utils/logger";
 
@@ -58,12 +59,20 @@ export class DockerManager {
    * Pull an image from its tag
    * @returns true in case of success
    */
-  public async pullImage(imageName: string): Promise<boolean> {
+  public async pullImage(imageName: string, containerLabels: DockerCiLabels): Promise<boolean> {
     try {
       const imageInfos = await this.getImage(imageName).inspect();
       this._logger.log("Pulling : ", ...imageInfos.RepoTags);
+      let authConf: DockerImagesModel.PullImageAuth | undefined;
+      if (containerLabels["docker-ci.username"] && containerLabels["docker-ci.password"] && containerLabels["docker-ci.auth-server"]) {
+        authConf = {
+          username: containerLabels["docker-ci.username"],
+          password: containerLabels["docker-ci.password"],
+          serveraddress: containerLabels["docker-ci.auth-server"]
+        }
+      }
       for(const tag of imageInfos.RepoTags)
-        await this._docker.pull(tag);
+        await this._docker.pull(tag, authConf && { authconfig: authConf });
       return true; 
     } catch (e) {
       this._logger.error("Error pulling image", e);
