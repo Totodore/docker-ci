8f4eded1a7a5153337e3a40d1e175cd9f38353db
totodore <prevottheodore@gmail.com>
2020-12-25 01:28:14 +0100

    adding api for optionnal hashed routes

--- src/webhooks.ts
+++ src/webhooks.ts
@@ -1,19 +1,29 @@
 import { Logger } from './utils/logger';
 import * as express from "express";
-
+import { HmacSHA256 } from "crypto-js";
+import * as basicAuth from "express-basic-auth";
 export class WebhooksManager {
-  public readonly routePath = "/deploy";
+  public readonly deployPath = "/deploy";
   private readonly _logger = new Logger(this);
 
-  private _app: express.Application = express();
-  private _router: express.Router = express.Router();
-  private readonly _routes: { [id: string]: () => void } = {};
+  private readonly _app: express.Application = express();
+  private readonly _router: express.Router = express.Router();
+  private readonly _routes: { name: string, handler: () => void, hash: string, id: string }[] = [];
+  private readonly _hashedRoutes = process.env.PROTECTED_WEBHOOKS == 'true';
+  private readonly _basicAuthConfig: basicAuth.BasicAuthMiddlewareOptions = {
+    users: {
+      [process.env.API_USER]: process.env.API_PASSWORD
+    },
+    challenge: true
+  }
 
   public async init() {
     this._app.use(this._router);
 
-    this._router.all(`${this.routePath}/:id`, (req: express.Request, res: express.Response) => this._webhookTriggered(req.params.id, res));
-
+    this._router.all(`${this.deployPath}/:id`, (req, res) => this._webhookTriggered(req.params.id, res));
+    if (this._hashedRoutes)
+      this._router.get(`/api/:name`, basicAuth(this._basicAuthConfig), (req, res) => this._apiGetUrlFromNameTriggered(req.params.name, res));
+    
     return new Promise<void>((resolve) => this._app.listen(process.env.PORT ?? 3000, () => resolve())).catch(e => {
       this._logger.error(e);
       this._logger.error("Error detected, stopping application...");
@@ -23,34 +33,50 @@ export class WebhooksManager {
 
   /**
    * Add a route to the list of routes
-   * @param id 
-   * @param handler 
    */
-  public addRoute(id: string, handler: () => void) {
-    this._routes[id] = handler;
+  public addRoute(name: string, id: string, handler: () => void): string {
+    const hash = this._hashedRoutes ? HmacSHA256(name, process.env.PRIVATE_KEY).toString() : name;
+    this._routes.push({
+      hash,
+      handler,
+      name,
+      id
+    });
+    return hash;
   }
 
   public removeRoute(id: string) {
-    this._routes[id] && delete this._routes[id];
+    const index = this._routes.findIndex(el => el.id == id);
+    this._routes.splice(index, 1);
   }
 
   /**
+   * Check if the hash given in argument is the good one
    * Check if the id in param exists, otherwise return a bad request 400
    * @param id 
    * @param res 
    */
-  private _webhookTriggered(id: string, res: express.Response) {
-    if (this._routes[id]) {
-      this._routes[id]();
-      res.status(200).send(`Webhook ${id} triggered...`);
-      this._logger.info(`${this.webhookUrl}/${id} triggered`);
+  private _webhookTriggered(hash: string, res: express.Response) {
+    const routeData = this._routes.find(el => this._hashedRoutes ? el.hash === hash : el.name === hash);
+    if (!routeData) {
+      this._logger.info("Webhook triggered with invalid hash");
+      res.status(400).send("Bad request : invalid hash");
+      return;
     }
-    else {
-      this._logger.error(`Unknown id route triggered : ${id}`);
-      res.status(400).send(`Unknown '${id}' webhook triggered...`);
-    }
+    routeData.handler();
+    res.status(200).send(`Webhook ${routeData.name} triggered...`);
+    this._logger.info(`${this.webhookUrl}/${routeData.name} triggered`);
+  }
+
+  private _apiGetUrlFromNameTriggered(name: string, res: express.Response) {
+    const data = this._routes.find(el => el.name == name);
+    this._logger.log(name);
+    if (data)
+      res.json({ name: data.name, url: this.webhookUrl + "/" + data.hash });
+    else
+      res.sendStatus(400);
   }
   
-  public get webhookUrl() { return `http(s)://0.0.0.0:${process.env.PORT ?? 3000}${this.routePath}`; };
+  public get webhookUrl() { return `http://0.0.0.0:${process.env.PORT ?? 3000}${this.deployPath}`; };
 
 }
