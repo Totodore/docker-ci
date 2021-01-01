007047e23220fa03329a98887547115a4a8f5065
totodore <prevottheodore@gmail.com>
2021-01-02 00:32:02 +0100

    update labels

--- src/models/docker-ci-labels.model.ts
+++ src/models/docker-ci-labels.model.ts
@@ -1,9 +1,14 @@
 export interface DockerCiLabels {
   "docker-ci.enable"?: string;
   "docker-ci.name"?: string;
-  "docker-ci.email"?: string;
   "docker-ci.repo-url"?: string;
+  
+  "docker-ci.email"?: string;
+
   "docker-ci.password"?: string;
   "docker-ci.username"?: string;
   "docker-ci.auth-server"?: string;
+
+  "docker-ci.webhook-secret"?: string;
+  "docker-ci.webhook-verify-url"?: string;
 }
